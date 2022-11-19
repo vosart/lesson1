84ef69ebe19645988fe104858f5a789d5ca198a0
Artem Voskresensky <metravs@gmail.com>
2022-11-20 00:46:51 +0300

    second commit

--- price.py
+++ price.py
@@ -1,7 +1,8 @@
 def format_price(price):
-    #price = int(price)
-    #return f'Цена: {price} руб.'
-    return 'Цена: {0} руб.'.format(int(price))
+    price = int(price)
+    return f'Цена: {price} руб.'
+    # в одну строку
+    # return 'Цена: {0} руб.'.format(int(price))
 
 new_price = format_price(56.24)
 print(new_price)
